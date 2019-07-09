6f71bce364d9021b28eb66f2e0938f7fc8f14de7
Erik Pehrers <erik.pehrers@jetshop.se>
2019-07-09 13:16:14 +0200

    added db connection, started on schema

---
+++ src/Schema.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+const productSchema = new Schema({});
--- src/server.js
+++ src/server.js
@@ -1,7 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const MongoClient = require('mongoose');
 const port = process.env.PORT || 7000;
+const connectionString = process.env.CONNECTION_STRING;
 
-const dbRoute = app.get('/', (request, response) => response.send('yello'));
+MongoClient.connect(connectionString, {
+  useNewUrlParser: true
+})
+  .then(res => res)
+  .catch(err => new Error(`No connected to db...) ${err}`));
+
+const db = MongoClient.connection;
+
+app.get('/', (request, response) => {
+  response.send('yello');
+  db.once('open', console.log('Connected'));
+});
 
 app.listen(port, () => console.log(`Server is listening on ${port}`));
