bc442812787a9427d8e88c8b1f98b3982444085d
Erik Pehrers <erik.pehrers@jetshop.se>
2019-07-09 17:01:19 +0200

    needed coors

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongoose');
 const Products = require('./Schema');
-const router = express.Router();
+const cors = require('cors');
 
 // ENV
 const port = process.env.PORT || 7000;
 const connectionString = process.env.CONNECTION_STRING;
+
+app.use(cors());
 
 MongoClient.connect(connectionString, {
   useNewUrlParser: true
@@ -18,8 +20,9 @@ MongoClient.connect(connectionString, {
   })
   .catch(err => new Error(`No connected to db...) ${err}`));
 
-const db = MongoClient.connection;
-db.once('open', () => Products.find((err, data) => console.log(data)));
+// Not used
+// const db = MongoClient.connection;
+// db.once('open', () => Products.find((err, data) => console.log(data)));
 
 function getAllProducts(response) {
   Products.find((err, data) => {
