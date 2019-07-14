6f06d72576ec55746b8431be0f9921bea076e894
Erik Pehrers <erik.pehrers@jetshop.se>
2019-07-14 13:58:00 +0200

    removes all REST usage files

--- src/routes/products.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const getAllProducts = require('../api/getProducts');
-
-router.get('/', (req, res) => getAllProducts(res));
-
-module.exports = router;
