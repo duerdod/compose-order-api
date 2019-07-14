4ab671c88efb294f2e8cdfed2609cf6f566b6b11
Erik Pehrers <erik.pehrers@jetshop.se>
2019-07-14 13:59:36 +0200

    more REST files

--- api/getProducts.js
+++
@@ -1,8 +0,0 @@
-const Products = require('../Schemas/Products');
-
-exports.getProducts = response => {
-  Products.find((err, products) => {
-    if (err) return response.json({ success: false, error: err });
-    return response.json({ success: true, products });
-  });
-};
--- api/index.js
+++
@@ -1,7 +0,0 @@
-const getProducts = require('./getProducts');
-const getProduct = require('./getProduct');
-
-module.exports = {
-  getProducts,
-  getProduct
-};
