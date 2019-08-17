e29c9c34668d2c957e62cef80184bed94359d83c
Erik Pehrers <erik.pehrers@jetshop.se>
2019-08-17 20:21:18 +0200

    more errors

--- src/server.js
+++ src/server.js
@@ -3,14 +3,12 @@ const express = require('express');
 const app = express();
 const graphServer = require('./graphServer');
 const server = graphServer();
-const path = require('path');
+// const path = require('path');
 
 // ENV
 const port = process.env.PORT || 8000;
 const hostname = process.env.HOSTNAME || '0.0.0.0';
 console.log(process.env.NODE_ENV);
-
-const clientPath = path.join(__dirname, '../../client');
 
 const corsOptions = {
   origin: '*',
@@ -29,21 +27,15 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Static files
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(clientPath, 'build')));
-  app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: path.join(clientPath, 'build') });
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(clientPath, 'build')));
+//   app.get('/*', (req, res) => {
+//     res.sendFile('index.html', { root: path.join(clientPath, 'build') });
+//   });
+// }
 
 // Apply Express as middleware to Apollo Server.
 server.applyMiddleware({ app, cors: corsOptions });
-
-// Start
-app.listen({ port, hostname }, () =>
-  console.log(`Listen to ${hostname}:${port}`)
-);
-// Middleware
 
 // Start server
 app.listen(() => {
